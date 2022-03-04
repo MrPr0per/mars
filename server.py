b8699aafeafedc382341c1397a41f3c6a76b578a
mr <proper>
2022-03-04 16:56:07 +0500

    добавлена карусель /carousel

--- server.py
+++ server.py
@@ -300,5 +300,46 @@ def page8():
         return "Форма отправлена"
 
 
+@app.route('/carousel')
+def page9():
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>фото фото фото</title>
+                      </head>
+                      <body>
+                        <h1>пейзажи марса</h1>
+                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                          <div class="carousel-inner">
+                            <div class="carousel-item active">
+                              <img src="{url_for('static', filename='img/panorama1.png')}" class="d-block w-100" alt="1">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="{url_for('static', filename='img/panorama1.png')}" class="d-block w-100" alt="2">
+                            </div>
+                            <div class="carousel-item">
+                              <img src="{url_for('static', filename='img/panorama1.png')}" class="d-block w-100" alt="3">
+                            </div>
+                          </div>
+                          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                          </button>
+                          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                          </button>
+                        </div>
+                      </body>
+                    </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
